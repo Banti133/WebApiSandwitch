d745175e01b4674085327cb3b78435b2a6567e59
Banti <bantimandal133@gmail.com>
2025-09-01 18:51:31 +0530

    updated authentication

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using WebApiSandwitch.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthorization();
+
+//activating identity apis
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,10 +16,15 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<SandwitchContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ApiConnection")));
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbCon")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>(); //mapping identity user
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
